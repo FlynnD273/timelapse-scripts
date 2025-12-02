f84f0b0dec2438d1609c5403a70bcf9ccf03bbcb
Flynn Duniho <flynn@harveyosity.org>
2025-12-02 07:44:47 -0800

    Add retry logic

--- takephoto.sh
+++ takephoto.sh
@@ -2,10 +2,11 @@
 
 cd /data/data/com.termux/files/home || exit
 file="storage/pictures/img.jpg"
+output="storage/pictures/Timelapse/$(date '+%F %HC%M').jpg"
 termux-camera-photo "$file"
-# size=$(wc -c <"$file")
-# if [ "$size" -le 3000000 ]; then
-# 	termux-camera-photo "$file"
-# fi
+size=$(wc -c <"$file")
+if [ "$size" -le 3000000 ]; then
+	termux-camera-photo "$file"
+fi
 
-mv "$file" "storage/pictures/Timelapse/$(date '+%F %HC%M').jpg"
+mv "$file" "$output"
