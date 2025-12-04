7c4ae03bf40f9f6cf51789130e70175045590ea2
Flynn Duniho <flynn@harveyosity.org>
2025-12-03 16:30:13 -0800

    Revert takephoto's size threshold

--- takephoto.sh
+++ takephoto.sh
@@ -5,7 +5,7 @@ file="storage/pictures/img.jpg"
 output="storage/pictures/Timelapse/$(date '+%F %HC%M').jpg"
 termux-camera-photo "$file"
 size=$(wc -c <"$file")
-if [ "$size" -le 2000000 ]; then
+if [ "$size" -le 3000000 ]; then
 	termux-camera-photo "$file"
 fi
 
