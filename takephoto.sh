c631441ffb87a4bb0dbf3c930406553f2dd027c7
Flynn Duniho <flynn@harveyosity.org>
2025-12-04 21:46:33 -0800

    Simplify photo-taking logic

--- takephoto.sh
+++ takephoto.sh
@@ -1,12 +1,9 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 cd /data/data/com.termux/files/home || exit
-file="storage/pictures/img.jpg"
-output="storage/pictures/Timelapse/$(date '+%F %HC%M').jpg"
+file="storage/pictures/Timelapse/$(date '+%F %HC%M').jpg"
 termux-camera-photo "$file"
 size=$(wc -c <"$file")
 if [ "$size" -le 3000000 ]; then
 	termux-camera-photo "$file"
 fi
-
-mv "$file" "$output"
